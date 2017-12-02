78ead0b75bc27a1ac2b262f4b74e25ddd7a5d389
fjkang <fjkang1991@gmail.com>
2017-12-02 13:41:47 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,15 +1,18 @@
 from nuomi import NuoMi
 from taopiaopiao import TaoPiaoPiao
 
-nuomi = NuoMi()
-# nuomi.get_sessions()
+nuomi = NuoMi() #创建糯米电影的类
+taopp = TaoPiaoPiao() #创建淘票票的类
 
-taopp = TaoPiaoPiao()
-# taopp.get_sessions()
-#
-# taopp.min_price_sessions()
+#获取场次信息的方法，数据存入数据库
+# nuomi.get_sessions()
+# taopp.get_sessions() 
+
+#获取最低价场次的方法
+# taopp.min_price_sessions() 
 # nuomi.min_price_sessions()
 
-nuomi.hot_movies()
-taopp.hot_movies()
+#获取热映电影的列表，并打印出来
+#nuomi.hot_movies() 
+#taopp.hot_movies()
 
